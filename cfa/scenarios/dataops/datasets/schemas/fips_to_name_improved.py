d588d0345a5df86a903d4edb0fa9e4a3da5aaa86
Ryan Raasch <xng3@cdc.gov>
2025-06-03 20:24:49 +0000

    precommit fixes

--- cfa/scenarios/dataops/datasets/schemas/fips_to_name_improved.py
+++ cfa/scenarios/dataops/datasets/schemas/fips_to_name_improved.py
@@ -3,16 +3,16 @@ import pandera.pandas as pa
 extract_schema = pa.DataFrameSchema(
     {
         "name": pa.Column(str),
-        "region": pa.Column(float, nullable = True),
-        "address": pa.Column(str, nullable = True),
-        "city": pa.Column(str, nullable = True),
-        "state": pa.Column(str, nullable = True),
-        "zipCode": pa.Column(str, nullable = True),
-        "states": pa.Column(object, nullable = True),
-        "loc": pa.Column(object, nullable = True),
-        "lastRefresh": pa.Column(str, nullable = True),
-        "hash": pa.Column(str, nullable = True),
-        "id": pa.Column(str, nullable = True),
+        "region": pa.Column(float, nullable=True),
+        "address": pa.Column(str, nullable=True),
+        "city": pa.Column(str, nullable=True),
+        "state": pa.Column(str, nullable=True),
+        "zipCode": pa.Column(str, nullable=True),
+        "states": pa.Column(object, nullable=True),
+        "loc": pa.Column(object, nullable=True),
+        "lastRefresh": pa.Column(str, nullable=True),
+        "hash": pa.Column(str, nullable=True),
+        "id": pa.Column(str, nullable=True),
     }
 )
 
