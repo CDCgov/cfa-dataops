120a5a6a2025fccb3c95b973854f1af3910a80e6
Ryan Raasch <xng3@cdc.gov>
2025-06-03 18:54:17 +0000

    allow raw fips nullable

--- cfa/scenarios/dataops/datasets/schemas/fips_to_name_improved.py
+++ cfa/scenarios/dataops/datasets/schemas/fips_to_name_improved.py
@@ -3,16 +3,16 @@ import pandera.pandas as pa
 extract_schema = pa.DataFrameSchema(
     {
         "name": pa.Column(str),
-        "region": pa.Column(int),
-        "address": pa.Column(str),
-        "city": pa.Column(str),
-        "state": pa.Column(str),
-        "zipCode": pa.Column(str),
-        "states": pa.Column(object),
-        "loc": pa.Column(object),
-        "lastRefresh": pa.Column(str),
-        "hash": pa.Column(str),
-        "id": pa.Column(str),
+        "region": pa.Column(int, nullable = True),
+        "address": pa.Column(str, nullable = True),
+        "city": pa.Column(str, nullable = True),
+        "state": pa.Column(str, nullable = True),
+        "zipCode": pa.Column(str, nullable = True),
+        "states": pa.Column(object, nullable = True),
+        "loc": pa.Column(object, nullable = True),
+        "lastRefresh": pa.Column(str, nullable = True),
+        "hash": pa.Column(str, nullable = True),
+        "id": pa.Column(str, nullable = True),
     }
 )
 
