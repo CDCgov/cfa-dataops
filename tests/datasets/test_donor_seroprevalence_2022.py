2f01d955b5933d68a54f48745be5e02d969003af
Ryan Raasch <xng3@cdc.gov>
2025-06-18 20:03:00 +0000

    more tests

---
+++ tests/datasets/test_donor_seroprevalence_2022.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import pandera.pandas as pa
+import pytest
+from pandera.errors import SchemaError
+
+from cfa.scenarios.dataops.datasets.schemas.donor_seroprevalence_2022 import (
+    extract_schema,
+    load_schema,
+    raw_synth_data,
+    tf_synth_data,
+)
+from cfa.scenarios.dataops.etl.donor_seroprevalence_2022 import transform
+
+
+def test_donor_seroprevalence_2022_schemas():
+    assert isinstance(extract_schema, pa.DataFrameSchema)
+    assert isinstance(load_schema, pa.DataFrameSchema)
+
+    # Check if the schemas have the expected columns
+    assert isinstance(extract_schema(raw_synth_data), pd.DataFrame)
+    assert isinstance(load_schema(tf_synth_data), pd.DataFrame)
+
+    with pytest.raises(SchemaError):
+        ex_cols = raw_synth_data.columns
+        extract_schema(
+            raw_synth_data.rename(
+                columns={col: col + "_renamed" for col in ex_cols}
+            )
+        )
+
+    with pytest.raises(SchemaError):
+        tf_cols = tf_synth_data.columns
+        load_schema(
+            tf_synth_data.rename(
+                columns={col: col + "_renamed" for col in tf_cols}
+            )
+        )
+
+
+def test_donor_seroprevalence_2022_transform():
+    extract_df = raw_synth_data
+    tf_df = transform(extract_df)
+
+    # Check if the transformed DataFrame matches the expected schema
+    assert isinstance(load_schema(tf_df), pd.DataFrame)
---
+++ tests/datasets/test_seroprevalence.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import pandera.pandas as pa
+import pytest
+from pandera.errors import SchemaError
+
+from cfa.scenarios.dataops.datasets.schemas.seroprevalence import (
+    extract_schema,
+    load_schema,
+    raw_synth_data,
+    tf_synth_data,
+)
+from cfa.scenarios.dataops.etl.seroprevalence import transform
+
+
+def test_seroprevalence_schemas():
+    assert isinstance(extract_schema, pa.DataFrameSchema)
+    assert isinstance(load_schema, pa.DataFrameSchema)
+
+    # Check if the schemas have the expected columns
+    assert isinstance(extract_schema(raw_synth_data), pd.DataFrame)
+    assert isinstance(load_schema(tf_synth_data), pd.DataFrame)
+
+    with pytest.raises(SchemaError):
+        ex_cols = raw_synth_data.columns
+        extract_schema(
+            raw_synth_data.rename(
+                columns={col: col + "_renamed" for col in ex_cols}
+            )
+        )
+
+    with pytest.raises(SchemaError):
+        tf_cols = tf_synth_data.columns
+        load_schema(
+            tf_synth_data.rename(
+                columns={col: col + "_renamed" for col in tf_cols}
+            )
+        )
+
+
+def test_seroprevalence_transform():
+    extract_df = raw_synth_data
+    tf_df = transform(extract_df)
+
+    # Check if the transformed DataFrame matches the expected schema
+    assert isinstance(load_schema(tf_df), pd.DataFrame)
