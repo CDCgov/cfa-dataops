cafbc5b955d3869f856338171cbd5a9c9d9b1440
cdc-ap66 <ap66@cdc.gov>
2025-08-18 16:24:39 +0000

    reporting functionality tests

---
+++ tests/test_reporting_catalog.py
@@ -0,0 +1,42 @@
+import os
+from types import SimpleNamespace
+
+from cfa.dataops.reporting import reportcat
+from cfa.dataops.reporting.catalog import NotebookEndpoint, get_report_catalog
+
+
+def test_catalog(data_dir):
+    """Test the report catalog."""
+    catalog = get_report_catalog()
+    assert catalog.__dict__.keys() == reportcat.__dict__.keys()
+    assert isinstance(catalog, SimpleNamespace)
+
+    # Check if a specific report exists
+    examples = catalog.examples
+    assert isinstance(examples, SimpleNamespace)
+    example_report = examples.basics_ipynb
+    assert isinstance(example_report, NotebookEndpoint)
+    assert example_report.notebook_path.endswith(".ipynb")
+    assert hasattr(example_report, "nb_to_html_str")
+    assert callable(example_report.nb_to_html_str)
+    assert hasattr(example_report, "nb_to_html_file")
+    assert callable(example_report.nb_to_html_file)
+    assert hasattr(example_report, "nb_to_html_blob")
+    assert callable(example_report.nb_to_html_blob)
+    assert hasattr(example_report, "notebook_path")
+    assert hasattr(example_report, "print_params")
+    assert callable(example_report.print_params)
+
+    # Test conversion to HTML string
+    html_content = example_report.nb_to_html_str()
+    assert isinstance(html_content, str)
+    assert (
+        "Basic Notebook Example" in html_content
+    )  # Basic check for HTML content
+
+    # Test saving to HTML file
+    html_out_path = os.path.join(data_dir, "test_output.html")
+    example_report.nb_to_html_file(html_out_path)
+    with open(html_out_path, "r") as f:
+        saved_content = f.read()
+        assert "Basic Notebook Example" in saved_content
