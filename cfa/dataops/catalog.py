155f52c433c376cd810e3b58be022633dab29158
cdc-ap66 <ap66@cdc.gov>
2025-09-15 18:30:04 +0000

    lots of updates including moving get_data() func to get_dataframe() method

--- cfa/dataops/catalog.py
+++ cfa/dataops/catalog.py
@@ -1,7 +1,8 @@
 """building a validated datasource namespace"""
 
-import glob
 import os
+import pkgutil
+from configparser import ConfigParser
 from importlib import import_module
 from io import BytesIO
 from types import SimpleNamespace
@@ -17,8 +18,44 @@ from cfa.cloudops.blob_helpers import (
     write_blob_stream,
 )
 
-from . import get_all_catalogs
-from .config_validator import validate_dataset_config, verify_no_repeats
+from .config_validator import (
+    ConfigValidator,
+    PropertiesValidation,
+    SourceValidation,
+    StorageEndpointValidation,
+    ValidationError,
+)
+from .reporting.catalog import report_dict_to_sn
+from .utils import get_dataset_dot_path
+
+_here = os.path.abspath(os.path.dirname(__file__))
+_config = ConfigParser()
+_config.read(os.path.join(_here, "config.ini"))
+
+
+def get_all_catalogs() -> list:
+    """Get a list of all available dataops catalogs.
+
+    Returns:
+        list[tuple]: A list of catalog names and paths.
+    """
+
+    catalogs = []
+    catalog_nspace = _config.get("DEFAULT", "catalog_namespaces")
+    try:
+        catalog_pkg = import_module(catalog_nspace)
+        for module_finder, modname, ispkg in pkgutil.iter_modules(
+            catalog_pkg.__path__
+        ):
+            if ispkg:
+                catalogs.append((catalog_nspace, modname, module_finder.path))
+    except ModuleNotFoundError as e:
+        raise ModuleNotFoundError(
+            f"No catalogs exist in namespace {catalog_nspace}"
+        ) from e
+
+    return catalogs
+
 
 all_catalogs = get_all_catalogs()
 
@@ -37,34 +74,8 @@ for cns, cat_name, cat_path in all_catalogs:
     for k in dataset_mod.dataset_ns_map.keys():
         all_defaults.update({k: defaults})
 
-
-_here_dir = os.path.split(os.path.abspath(__file__))[0]
-_dataset_config_paths = glob.glob(
-    os.path.join(_here_dir, "**", "*.toml"), recursive=True
-)
-
-name_paths = []
-dataset_configs = {}
-for cp_i in _dataset_config_paths:
-    if cp_i.startswith(os.path.join(_here_dir, "datasets")):
-        ns_pre = cp_i.split(f"datasets{os.sep}")[-1].split(os.sep)[:-1]
-        with open(cp_i, "rb") as f:
-            config = tomli.load(f)
-            config["_metadata"] = dict(
-                filename=os.path.split(cp_i)[1], config_path=cp_i
-            )
-            validate_dataset_config(config)
-            current = dataset_configs
-            for part in ns_pre:
-                if part not in current:
-                    current[part] = {}
-                current = current[part]
-            current[config["properties"]["name"]] = config
-            name_paths.append(
-                ".".join(ns_pre + [config["properties"]["name"]])
-            )
-
-verify_no_repeats(name_paths)
+dataset_namespaces = get_dataset_dot_path(all_dataset_ns_map)
+report_namespaces = get_dataset_dot_path(all_reports_ns_map)
 
 
 class DatasetEndpoint:
@@ -87,17 +98,43 @@ class DatasetEndpoint:
                 account = v.get("account", "")
                 container = v.get("container", "")
                 if account == "":
-                    account = self.defaults["storage"]["account"]
+                    self.config[k]["account"] = self.defaults["storage"][
+                        "account"
+                    ]
                 if container == "":
-                    container = self.defaults["storage"]["container"]
+                    self.config[k]["container"] = self.defaults["storage"][
+                        "container"
+                    ]
+        self.validate_dataset_config(config_path)
+        for k, v in self.config.items():
+            if k in ["load", "extract"] or k.startswith("stage"):
                 self.__setattr__(
                     k,
                     BlobEndpoint(
-                        account=account,
-                        container=container,
+                        account=self.config[k]["account"],
+                        container=self.config[k]["container"],
                         prefix=v["prefix"],
                     ),
                 )
+
+    def validate_dataset_config(self, config_path) -> None:
+        """Validate the dataset configuration using ConfigValidator."""
+        try:
+            config_models = {}
+            for c_key, c_value in self.config.items():
+                if (
+                    c_key.startswith("stage_") or c_key in ["load", "extract"]
+                ) and c_value is not None:
+                    config_models[c_key] = StorageEndpointValidation(**c_value)
+                elif c_key == "properties":
+                    config_models[c_key] = PropertiesValidation(**c_value)
+                elif c_key == "source":
+                    config_models[c_key] = SourceValidation(**c_value)
+                else:
+                    config_models[c_key] = c_value
+            ConfigValidator(**config_models)
+        except ValidationError as e:
+            raise ValueError(f"Invalid dataset {config_path}: {e}") from e
 
 
 class BlobEndpoint:
@@ -206,6 +243,60 @@ class BlobEndpoint:
             key=lambda x: x["creation_time"],
         )
 
+    def get_dataframe(
+        self, output="pandas", version="latest"
+    ) -> pd.DataFrame | pl.DataFrame:
+        """Get the data as a pandas or polars dataframe
+
+        Args:
+            output (str, optional): the type of dataframe to return,
+            either 'pandas' or 'polars'. Defaults to "pandas".
+            version (str, optional): the version of the data to get.
+            Defaults to "latest".
+
+        Raises:
+            ValueError: if output is not 'pandas' or 'polars'
+
+        Returns:
+            pd.DataFrame | pl.DataFrame: the dataframe
+        """
+        if output not in ["pandas", "polars", "pd", "pl"]:
+            raise ValueError(
+                f"Output {output} needs to be 'pandas', 'polars', 'pd, or 'pl'."
+            )
+        blobs = self.read_blobs(version)
+        file_ext = self.get_file_ext()
+        if file_ext == "csv":
+            if output in ["pandas", "pd"]:
+                df = pd.concat([pd.read_csv(blob) for blob in blobs])
+            else:
+                df = pl.concat(
+                    [pl.read_csv(blob.content_as_bytes()) for blob in blobs],
+                    how="vertical_relaxed",
+                )
+            return df
+        elif file_ext == "json":
+            if output in ["pandas", "pd"]:
+                df = pd.concat([pd.read_json(blob) for blob in blobs])
+            else:
+                df = pl.concat(
+                    [pl.read_json(blob.content_as_bytes()) for blob in blobs],
+                )
+            return df
+        elif file_ext == "parquet" or file_ext == "parq":
+            pq_bytes = [blob.content_as_bytes() for blob in blobs]
+            pq_files = [BytesIO(pq) for pq in pq_bytes]
+            if output in ["pandas", "pd"]:
+                df = pd.concat(
+                    [pd.read_parquet(pq_file) for pq_file in pq_files]
+                )
+            else:
+                df = pl.concat(
+                    [pl.read_parquet(pq_file) for pq_file in pq_files],
+                    how="vertical_relaxed",
+                )
+            return df
+
 
 def dict_to_sn(d: Any, defaults: dict = None) -> SimpleNamespace:
     """Simple recursive namespace construction
@@ -238,118 +329,15 @@ dc = []
 for k in all_dataset_ns_map.keys():
     dc.append(dict_to_sn({k: all_dataset_ns_map[k]}, all_defaults.get(k, {})))
 combined_dict = {key: value for ns in dc for key, value in vars(ns).items()}
+
+rc = []
+for k in all_reports_ns_map.keys():
+    rc.append(report_dict_to_sn({k: all_reports_ns_map[k]}))
+combined_reports_dict = {
+    key: value for ns in rc for key, value in vars(ns).items()
+}
+
 datacat = SimpleNamespace(**combined_dict)
-
-
-def get_data(
-    name: str,
-    version="latest",
-    type="transformed",
-    output="pandas",
-) -> pd.DataFrame | pl.DataFrame:
-    """
-    Gets the data from blob storage based on provided parameters
-
-    Args:
-        name (str): name of dataset
-        version (str, optional): version of dataset. Defaults to "latest".
-        type (str, optional): type of data, either 'raw' or 'transformed'. Defaults to "transformed".
-        output (str, optional): dataframe output type, either 'pandas' or 'polars'. Defaults to "pandas".
-
-    Returns:
-        pd.DataFrame | pl.DataFrame: pandas or polars dataframe
-    """
-    # check data exists
-    try:
-        config = eval(f"datacat.{name}")
-    except AttributeError as e:
-        raise ValueError(
-            f"{name} not in available datasets."
-            f" Available datasets: {list_datasets()}"
-        ) from e
-
-    # validate type, raise error if not raw or transformed
-    if type not in ["raw", "transformed"]:
-        raise ValueError(f"Type {type} needs to be 'raw' or 'transformed'.")
-
-    # validate output, raise error if not pandas or polars
-    if output not in ["pandas", "polars", "pd", "pl"]:
-        raise ValueError(
-            f"Output {output} needs to be 'pandas', 'polars', 'pd, or 'pl'."
-        )
-
-    # continue workflow based on raw or transformed
-    if type == "raw":
-        # get the BlobEndpoint for the raw data
-        blob_endpoint = config.extract
-        # check version exists, raise error if not
-        if version != "latest":
-            v_list = blob_endpoint.get_versions()
-            # check if version is in the list of available versions
-            if version not in v_list:
-                print(f"Version {version} not in available versions.")
-                print("Available versions:", v_list)
-                raise ValueError(
-                    f"Version {version} not in available versions."
-                )
-        # get blobs and convert to correct df
-        if config.extract.get_file_ext() == "csv":
-            blobs = blob_endpoint.read_blobs()
-            if output in ["pandas", "pd"]:
-                df = pd.concat([pd.read_csv(blob) for blob in blobs])
-            else:
-                df = pl.concat(
-                    [pl.read_csv(blob.content_as_bytes()) for blob in blobs],
-                    how="vertical_relaxed",
-                )
-            return df
-        elif config.extract.get_file_ext() == "json":
-            blobs = blob_endpoint.read_blobs()
-            if output in ["pandas", "pd"]:
-                df = pd.concat(
-                    [pd.read_json(blob.content_as_bytes()) for blob in blobs]
-                )
-            else:
-                df = pl.concat(
-                    [pl.read_json(blob.content_as_bytes()) for blob in blobs],
-                )
-            return df
-    else:
-        blob_endpoint = config.load
-        # check version exists, raise error if not
-        if version != "latest":
-            v_list = blob_endpoint.get_versions()
-            if version not in v_list:
-                print(f"Version {version} not in available versions.")
-                print("Available versions:", v_list)
-                raise ValueError(
-                    f"Version {version} not in available versions."
-                )
-        # get blobs and convert to correct df
-        blobs = blob_endpoint.read_blobs()
-        pq_bytes = [blob.content_as_bytes() for blob in blobs]
-        pq_files = [BytesIO(pq) for pq in pq_bytes]
-        if output in ["pandas", "pd"]:
-            df = pd.concat([pd.read_parquet(pq_file) for pq_file in pq_files])
-        else:
-            df = pl.concat(
-                [pl.read_parquet(pq_file) for pq_file in pq_files],
-                how="vertical_relaxed",
-            )
-        return df
-
-
-def list_datasets() -> list[str]:
-    """
-    Lists all available datasets in the catalog
-
-    Returns:
-        list[str]: list of dataset names
-
-    Examples:
-        >>> datasets = list_datasets()
-        >>> 'scenarios.covid19vax_trends' in datasets
-        True
-
-    """
-    return name_paths
+datacat.__setattr__("__namespace_list__", dataset_namespaces)
+reportcat = SimpleNamespace(**combined_reports_dict)
+reportcat.__setattr__("__namespace_list__", report_namespaces)
